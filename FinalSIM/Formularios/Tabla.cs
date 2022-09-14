ef174375060afbc7dc0539fdbe299e6b93a1bfff
Esteban <diazesteban600@gmail.com>
2022-09-13 22:18:04 -0300

    proceso de calcular nueva fila comenzando

---
+++ FinalSIM/Formularios/Tabla.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalSIM.Formularios
+{
+    public partial class Tabla : Form
+    {
+        public Tabla()
+        {
+            InitializeComponent();
+        }
+    }
+}
